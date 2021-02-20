795d4daf8269f83656d5e20386e130ee4c6b0b06
Saurav Chakraborty <38391547+sauravchakraborty13069@users.noreply.github.com>
2021-02-20 13:48:02 +0530

    Update clientApp.py

--- clientApp.py
+++ clientApp.py
@@ -41,4 +41,4 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     #app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
